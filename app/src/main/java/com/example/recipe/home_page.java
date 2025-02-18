513a7a81f112b6dfa971521769643ca45910c51d
shashank <gudimetlashashank999@gmail.com>
2025-02-18 10:13:35 +0530

    code1

--- app/src/main/java/com/example/recipe/home_page.java
+++ app/src/main/java/com/example/recipe/home_page.java
@@ -14,6 +14,6 @@ public class home_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-ewrf
+
         }
     }
